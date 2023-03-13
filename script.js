35e6fa1f4e00e449098548cf7059eda2dab276d2
Def <deferenseo@gmail.com>
2023-03-13 17:29:51 +0300

    added leaflet lib to progect

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/* Обьявление необходимых переменных и констант */
 const form = document.querySelector('.form');
 const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
@@ -7,3 +8,32 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--temp');
 const inputElevation = document.querySelector('.form__input--climb');
+
+
+/* Получение широты и долготы текущего местоположения */
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(
+        function (position) {
+            const { latitude, longitude } = position.coords;
+            console.log(`https://www.google.com/maps/@${latitude},${longitude},13z`);
+
+            const coords = [latitude, longitude];
+
+            /* Отображение карты через leaflet */
+            const map = L.map('map').setView(coords, 13); // второй параметр - zoom
+
+            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker(coords).addTo(map)
+                .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+                .openPopup();
+        },
+        function () {
+            alert('Невозможно получить вашe местоположение');
+        }
+    );
+}
+
+
