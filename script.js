e1945c69360daf5c57492520a83b373815a88931
Def <deferenseo@gmail.com>
2023-03-13 21:48:52 +0300

    refactored app

--- script.js
+++ script.js
@@ -6,51 +6,79 @@ const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
-const inputCadence = document.querySelector('.form__input--temp');
-const inputElevation = document.querySelector('.form__input--climb');
+const inputTemp = document.querySelector('.form__input--temp');
+const inputClimb = document.querySelector('.form__input--climb');
 
 
-/* Получение широты и долготы текущего местоположения */
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(
-        function (position) {
-            const { latitude, longitude } = position.coords;
-            console.log(`https://www.google.com/maps/@${latitude},${longitude},13z`);
+class App {
+    #map;
+    #mapEvent;
 
-            const coords = [latitude, longitude];
-
-            /* Отображение карты через leaflet */
-            const map = L.map('map').setView(coords, 13); // второй параметр - zoom
-
-            L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
-
-
-            map.on('click', function (mapEvent) {
-                console.log(mapEvent);
-
-                /* Получение широты и долготы маркера */
-                const { lat, lng } = mapEvent.latlng;
-
-                /* Установка настроек маркера */
-                L.marker([lat, lng])
-                    .addTo(map)
-                    .bindPopup(L.popup({
-                        autoClose: false,
-                        closeOnClick: false,
-                        className: 'running-popup'
-                    }))
-                    .setPopupContent('Тренировка')
-                    .openPopup();
-            });
-        },
-
-        /* Ошибка при неуспешной загрузки карты */
-        function () {
-            alert('Невозможно получить вашe местоположение');
+    constructor() {
+        this._getPosition();
+        form.addEventListener('submit', this._newWorkout.bind(this));
+        inputType.addEventListener('change', this._toggleClimbField);
+    }
+    _getPosition() {
+        /* Получение широты и долготы текущего местоположения */
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                this._loadMap.bind(this),
+                /* Ошибка при неуспешной загрузки карты */
+                function () {
+                    alert('Невозможно получить вашe местоположение');
+                }
+            );
         }
-    );
+    }
+    _loadMap(position) {
+        const { latitude, longitude } = position.coords;
+        console.log(`https://www.google.com/maps/@${latitude},${longitude},13z`);
+
+        const coords = [latitude, longitude];
+        /* Отображение карты через leaflet */
+        console.log(this);
+        this.map = L.map('map').setView(coords, 13); // второй параметр - zoom
+
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(this.map);
+
+        // Обработка клика на карте
+        this.map.on('click', this._showForm.bind(this));
+    }
+    _showForm(e) {
+        this.#mapEvent = e;
+        form.classList.remove('hidden');
+        inputDistance.focus();
+    }
+
+    _toggleClimbField() {
+        inputClimb.closest('.form__row').classList.toggle('form__row--hidden');
+        inputTemp.closest('.form__row').classList.toggle('form__row--hidden');
+    }
+
+    _newWorkout(e) {
+        e.preventDefault();
+        // Очистка полей ввода данных
+        inputDistance.value = inputDuration.value = inputTemp.value = inputClimb.value = '';
+        // Получение широты и долготы маркера 
+        const { lat, lng } = this.#mapEvent.latlng;
+
+        // Установка настроек маркера 
+        L.marker([lat, lng])
+            .addTo(this.map)
+            .bindPopup(L.popup({
+                autoClose: false,
+                closeOnClick: false,
+                className: 'running-popup'
+            }))
+            .setPopupContent('Тренировка')
+            .openPopup();
+    }
 }
+
+const app = new App();
+
 
 
